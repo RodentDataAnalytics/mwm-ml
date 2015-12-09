777bbac8a6da65b020fdf37b6d7f5fdff94062f0
Tiago Gehring <tiagogehring@gmail.com>
2015-12-09 23:08:48 +0100

    Removed unused configs

--- initialize.m
+++ initialize.m
@@ -16,12 +16,7 @@ addpath(fullfile(fileparts(mfilename('fullpath')),'/config/place_avoidance'));
 
 global g_config;
 %% Change for desired configuration
-% g_config = config_place_avoidance(config_place_avoidance.DATA_SET_LARGE); 
-% g_config = config_place_avoidance_mem; 
-% g_config = config_place_avoidance_darkness1; 
 g_config = config_mwm; 
-% g_config = config_mwm_nencki;
-% g_config = config_mwm_nencki_short;
 
 fprintf('Configuration selected: %s\n', g_config.DESCRIPTION);
 addpath(fullfile(fileparts(mfilename('fullpath')), g_config.RESULTS_DIR)); 
