c98f0b7669a1cf0a7423dbf506ae4c2500837012
Tiago Gehring <tiagogehring@gmail.com>
2016-02-28 23:31:06 +0100

    Found last differences for reproducing paper results

--- cache_animals.m
+++ cache_animals.m
@@ -17,65 +17,61 @@ function cache_animals
 
         g_animals_ids = {};
         g_animals_trajectories_map = {};
-        for g = 1:g_config.GROUPS
-            % select ids based on first trial            
+        for g = 1:2
+            % select ids based on first trial
             map = find(g_trajectories_group == g & g_trajectories_trial == 1);
             ids = arrayfun( @(x) x.id, g_trajectories.items(map));
             for t = 2:g_config.TRIALS
                 trial_idx = find(g_trajectories_group == g & g_trajectories_trial == t);
-                ids2 = arrayfun( @(x) x.id, g_trajectories.items(trial_idx));                                
-                for tmp = 1:length(trial_idx)
-                    pos = find(ids == ids2(tmp));
-                    if ~isempty(pos)
-                        map(t, pos) = trial_idx(tmp);
-                    end                   
-                end
+                trial_ids = arrayfun( @(x) x.id, g_trajectories.items(trial_idx));
+                map = [map; arrayfun( @(id) trial_idx(trial_ids == id), ids)];
             end
-            if g_config.REGULARIZE_GROUPS && g_config.GROUPS == 2 % TODO: generalize this for more than 2 groups; or maybe not              
-                avg_speed = mean(g_trajectories_speed(map));
-                [~, ord] = sort(avg_speed);                        
-                if g == 1
-                    nd = g_animals_count(1) - g_animals_count(2);
-                    if nd > 0
-                        nd = max(nd, g_config.NDISCARD);
-                    else
-                        nd = max(g_config.NDISCARD + nd, 0);
-                    end
-                    for s = 1:g_config.SESSIONS                    
-                        ti = (s - 1)*g_config.TRIALS_PER_SESSION + 1;
-                        tf = s*g_config.TRIALS_PER_SESSION;
-                        avg_speed = mean(g_trajectories_speed(map(ti:tf, :)));
-                        [~, ord] = sort(avg_speed);
-
-                    end
-                    ids = ids(ord(1:length(ord) - nd));
-                    map = map(:, ord(1:length(ord) - nd));
+            avg_speed = mean(g_trajectories_speed(map));
+            [~, ord] = sort(avg_speed);
+            if g == 1
+                nd = g_animals_count(1) - g_animals_count(2);
+                if nd > 0
+                    nd = max(nd, g_config.NDISCARD);
                 else
-                    nd = g_animals_count(2) - g_animals_count(1);
-                    if nd > 0
-                        nd = max(nd, g_config.NDISCARD);
-                    else
-                        nd = max(g_config.NDISCARD + nd, 0);
-                    end
-
-                    for s = 1:g_config.SESSIONS                    
-                        ti = (s - 1)*g_config.TRIALS_PER_SESSION + 1;
-                        tf = s*g_config.TRIALS_PER_SESSION;
-                        avg_speed = mean(g_trajectories_speed(map(ti:tf, :)));
-                        [~, ord] = sort(avg_speed);
-
-                        % discard N too calm animals                
-                        idx = map(ti:tf, ord(1:nd));                
-                        g_trajectories_group(idx(:)) = -2;                
-                    end
-
-                    ids = ids(ord(nd + 1:length(ord)));
-                    map = map(:, ord(nd + 1:length(ord)));                    
+                    nd = max(g_config.NDISCARD + nd, 0);
                 end
-            end
+                for s = 1:g_config.SESSIONS                    
+                    ti = (s - 1)*g_config.TRIALS_PER_SESSION + 1;
+                    tf = s*g_config.TRIALS_PER_SESSION;
+                    avg_speed = mean(g_trajectories_speed(map(ti:tf, :)));
+                    [~, ord] = sort(avg_speed);
+                    
+                    % discard N too stressed animals                
+                    idx = map(ti:tf, ord(length(ord) - nd + 1:end));
+                    g_trajectories_group(idx(:)) = -1;
+                end
+                ids = ids(ord(1:length(ord) - nd));
+                map = map(:, ord(1:length(ord) - nd));
+            else
+                nd = g_animals_count(2) - g_animals_count(1);
+                if nd > 0
+                    nd = max(nd, g_config.NDISCARD);
+                else
+                    nd = max(g_config.NDISCARD + nd, 0);
+                end
+                
+                for s = 1:g_config.SESSIONS                    
+                    ti = (s - 1)*g_config.TRIALS_PER_SESSION + 1;
+                    tf = s*g_config.TRIALS_PER_SESSION;
+                    avg_speed = mean(g_trajectories_speed(map(ti:tf, :)));
+                    [~, ord] = sort(avg_speed);
+                    
+                    % discard N too calm animals                
+                    idx = map(ti:tf, ord(1:nd));                
+                    g_trajectories_group(idx(:)) = -2;                
+                end
 
+                ids = ids(ord(nd + 1:length(ord)));
+                map = map(:, ord(nd + 1:length(ord)));                    
+            end
+            
             g_animals_ids = [g_animals_ids, ids];
-            g_animals_trajectories_map = [g_animals_trajectories_map, map];                                        
+            g_animals_trajectories_map = [g_animals_trajectories_map, map];                                                                                     
         end
     end
 end
--- features/trajectory_average_speed.m
+++ features/trajectory_average_speed.m
@@ -1,3 +1,15 @@
 function [ spd ] = trajectory_average_speed( traj, varargin )                                                  
-    spd = trajectory_length(traj, varargin{:}) / trajectory_latency(traj);
+    [repr, min_len ] = process_options(varargin, 'DataRepresentation', 1, 'MinLength', 1);
+    pts = traj.data_representation(repr);       
+    
+    temp = [];
+    for i = 2:length(pts)
+        % compute the length in cm and seconds
+        len = norm( pts(i, 2:3) - pts(i-1, 2:3) );
+        if len > min_len % discard points which are too close together
+            dt = pts(i, 1) - pts(i - 1, 1);
+            temp = [temp, len / dt];
+        end
+    end 
+    spd = mean(temp);
 end
