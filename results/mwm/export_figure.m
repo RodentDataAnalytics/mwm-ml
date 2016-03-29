c09009d68b6b8f79b5baa8986ea50f438d5067a9
Avgoustinos Vouros <av.vouros@gmail.com>
2016-03-29 11:59:58 +0100

    Update export_figure.m

--- results/mwm/export_figure.m
+++ results/mwm/export_figure.m
@@ -16,30 +16,27 @@
 %
 % EXAMPLE_2: export_figure(0, gcf, 'C:\', 'myfig_2'); -> 
 %            requests a user's input:
-%                1 = .fig file, 2 = .eps file, 3 = export_fig .eps file
+%                1 = .fig file, 2 = .eps file
 %            generates one of the following:
-%                C:\myfig_1.fig OR C:\myfig_1.eps OR C:\myfig_1.eps (paper quality)
+%                C:\myfig_1.fig OR C:\myfig_1.eps
 
 
 function export_figure(flag, get_current_figure, get_path, get_name)
 
     if flag == 1
-       % saveas(get_current_figure, strcat(get_path,get_name,'.fig')); %default export format
+        saveas(get_current_figure, strcat(get_path,get_name,'.fig')); %default export format
         saveas(get_current_figure, strcat(get_path,get_name,'.eps'),'epsc2');
+        % we need to specify eps color (epsc or epsc2) as the format
+        % parameter or else the exported picture will be black & white
     else
         while (1)
-            prompt = sprintf('Exporting...: \n1. .fig format \n2. .eps format \n3. High quality .eps format (requires external library) \nChoice: ');
+            prompt = sprintf('Exporting...: \n1. .fig format \n2. .eps format \nChoice: ');
             user_input = input(prompt)
             if user_input == 1
                 saveas(get_current_figure, strcat(get_path,get_name,'.fig'));
                 break;
             elseif user_input == 2
                 saveas(get_current_figure, strcat(get_path,get_name,'.eps'),'epsc2');
-                break;
-                % we need to specify eps color (epsc or epsc2) as the format
-                % parameter or else the exported picture will be black & white
-            elseif user_input == 3
-                export_fig(fullfile(get_path, strcat(get_name,'.eps')));
                 break;
             else
                 prompt = 'Wrong input'
