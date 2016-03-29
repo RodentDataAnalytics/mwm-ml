0d50d976a793ea51d5fe63337f5c47f3f5f78d32
Avgoustinos Vouros <av.vouros@gmail.com>
2016-03-29 12:03:55 +0100

    Update results_calibration.m

--- results/mwm/results_calibration.m
+++ results/mwm/results_calibration.m
@@ -1,7 +1,12 @@
+% Produces the Calibration functions (x and y position correction) and the
+% Calibration error as a function of the number of calibration points.
+
+% Publication:
+% Supplementary Material
+% page 13 Figure 8 & page 14 Figure 10
+
 function results_calibration
-%RESULTS_CALIBRATION Summary of this function goes here
-%   Detailed explanation goeshere
-% show calibration results   
+
     global g_config;    
                 
     [traj, cal_data] = load_trajectories(1:3, 1);
@@ -23,7 +28,6 @@ function results_calibration
         zlabel('correction [cm]');
         set(gcf, 'Color', 'w');
         set(gca, 'FontSize', g_config.FONT_SIZE, 'LineWidth', g_config.AXIS_LINE_WIDTH);
-        %%export_fig(fullfile(g_config.OUTPUT_DIR, sprintf('calibration_set%d_x.eps', i)));
         export_figure(1, gcf, g_config.OUTPUT_DIR, sprintf('calibration_set%d_x', i));
         
         figure('name', sprintf('Calibration function Y (set %d)', i));
@@ -33,7 +37,6 @@ function results_calibration
         zlabel('correction [cm]');
         set(gcf, 'Color', 'w');
         set(gca, 'FontSize', g_config.FONT_SIZE, 'LineWidth', g_config.AXIS_LINE_WIDTH);
-        %%export_fig(fullfile(g_config.OUTPUT_DIR, sprintf('calibration_set%d_y.eps', i)));
         export_figure(1, gcf, g_config.OUTPUT_DIR, sprintf('calibration_set%d_y', i));
         
         % cross validation        
@@ -67,84 +70,6 @@ function results_calibration
         errorbar(pts(:,1), pts(:,2), pts(:,3), 'k:', 'LineWidth', g_config.LINE_WIDTH);
         xlabel('number of calibration points');
         ylabel('error [cm]');        
-        %%export_fig(fullfile(g_config.OUTPUT_DIR, sprintf('calibration_error%d.eps', i)));
         export_figure(1, gcf, g_config.OUTPUT_DIR, sprintf('calibration_error%d', i));
     end
-    
-%     traj_cal = [];
-%     traj_uncal = [];
-%     traj_name = {};
-%     for i = 1:length(g_config.TRAJECTORY_SNAPSHOTS_DIRS)
-%         samples = dir(strcat(g_config.TRAJECTORY_SNAPSHOTS_DIRS{i}, '*.png')); 
-%         if length(samples) > 1
-%             figure('name', sprintf('Samples for set %d', i));
-%         end                
-%         for j = 1:length(samples)
-%             temp = sscanf(samples(j).name, 'day%d_track%d.png');
-%             day = temp(1);
-%             track = temp(2);
-%             
-%             traj_name = [traj_name, sprintf('set%d_%s', i, samples(j).name)];
-% 
-%             % look for corresponding trajectory in the uncorrected data set
-%             idxorig = -1;
-%             for k = 1:length(origtraj)
-%                 id1 = origtraj.items(k).data_identification();                            
-%                 if i == id1(1) && day == id1(2) && track == id1(3)
-%                    % found ya                
-%                    idxorig = k;
-%                    break;
-%                 end
-%             end    
-%             % save index for later plotting as well
-%             traj_uncal = [traj_uncal, idxorig];            
-% 
-%             idx = -1;
-%             for k = 1:length(traj)
-%                 id1 = traj.items(k).data_identification();                            
-%                 if i == id1(1) && day == id1(2) && track == id1(3)                
-%                    % found ya                
-%                    idx = k;
-%                    break;
-%                 end
-%             end    
-%             % save index for later plotting as well
-%             traj_cal = [traj_cal, idx];            
-% 
-%             if idx == -1 || idxorig == -1
-%                 error('Did not find trajectory');
-%             end
-% 
-%             subaxis(length(samples), 3, (j-1)*3 + 1, 'Spacing', 0.03, 'Padding', 0, 'Margin', 0.05);        
-%             imshow(strcat(g_config.TRAJECTORY_SNAPSHOTS_DIRS{i}, samples(i).name), 'Border', 'tight');            
-%             subaxis(length(samples), 3, (j-1)*3 + 2, 'Spacing', 0.03, 'Padding', 0, 'Margin', 0.05);     
-%             origtraj(idxorig).plot;                        
-%             subaxis(length(samples), 3, (j-1)*3 + 3, 'Spacing', 0.03, 'Padding', 0, 'Margin', 0.05);    
-%             traj(idx).plot;            
-%         end            
-%     end
-%     
-%     % export individual figures
-%     for i = 1:length(traj_uncal)
-%         figure(111);        
-%         clf;                        
-%         origtraj(traj_uncal(i)).plot;
-%         set(gcf, 'Color', 'w');
-%         set(gca,'DataAspectRatio',[1 1 1], 'PlotBoxAspectRatio',[1 1 1]);
-%         export_fig(fullfile(g_config.OUTPUT_DIR, sprintf('%s_uncalibrated.eps', traj_name{i})));       
-%     end
-%     
-%     for i = 1:length(traj_cal)
-%         figure(111);
-%         clf;
-%         traj(traj_cal(i)).plot;
-%         set(gcf, 'Color', 'w');
-%         set(gca,'DataAspectRatio',[1 1 1], 'PlotBoxAspectRatio',[1 1 1]);
-%         export_fig(fullfile(g_config.OUTPUT_DIR, sprintf('%s_calibrated.eps', traj_name{i})));       
-%     end
-%     
-%     for i = 1:length(cal_data) 
-%         temp = cal_data{i};
-%         fprintf('Total calibration points for set %d: %d', i, size(temp, 1));
-%     end
 end
