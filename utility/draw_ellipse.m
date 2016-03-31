dacd7db3c3e2775e5c0ddccfb9ce1b97d87b88d7
Avgoustinos Vouros <av.vouros@gmail.com>
2016-03-31 15:05:38 +0100

    draw_ellipse.m, added comments

--- utility/draw_ellipse.m
+++ utility/draw_ellipse.m
@@ -1,11 +1,10 @@
 function draw_ellipse(xc, yc, a, b, inc, style)
-%DRAW_ELLIPSE Summary of this function goes here
-%   Detailed explanation goes here
+%DRAW_ELLIPSE draws an elipse with center in (xc,yc), semiaxes (a, b) and rotation (inc) in radians.
     t = linspace(0,2*pi);
     px = a*cos(t);
     py = b*sin(t);
     inc = inc;
     x = xc + px*cos(inc) - py*sin(inc);
     y = yc + px*sin(inc) + py*cos(inc);
-    plot(x, y, '-r');
+    plot(x, y, '-r'); % '-r' can be replaced with 'style' for manually configuration of linestyle
 end
