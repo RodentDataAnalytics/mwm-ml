dadae54155df7549760c9cc0a3b9442e1a85f3d9
Avgoustinos Vouros <av.vouros@gmail.com>
2016-03-31 18:48:58 +0100

    results_export_selected_trajectories.m comments

--- results/mwm/results_export_selected_trajectories.m
+++ results/mwm/results_export_selected_trajectories.m
@@ -1,9 +1,10 @@
 function results_export_selected_trajectories
 %RESULTS_EXPORT_SELECTED Export some trajectories/segments of interest
-    global g_segments;    
-    global g_trajectories;    
-    global g_config;
-    global g_long_trajectories_map;
+    
+    global g_segments; %total segments produced from the splitting of trajectories    
+    global g_trajectories; %total trajectories   
+    global g_config; %configurations
+    global g_long_trajectories_map; %keeps only the trajectory with length > 0
     % initialize data
     cache_trajectory_segments;                    
 
@@ -23,33 +24,10 @@ function results_export_selected_trajectories
         for i = 1:length(pos)
            g_trajectories.items(pos(i)).plot;
            set(gcf, 'Color', 'w');
-           %fn = fullfile(g_config.OUTPUT_DIR, sprintf('/trajectory_s%d_d%d_t%d.eps', g_trajectories.items(pos(i)).set, g_trajectories.items(pos(i)).session, g_trajectories.items(pos(i)).track));
-           %export_fig(fn);  
            export_figure(1, gcf, g_config.OUTPUT_DIR, sprintf('/trajectory_s%d_d%d_t%d', g_trajectories.items(pos(i)).set, g_trajectories.items(pos(i)).session, g_trajectories.items(pos(i)).track));
         end       
     end
-    
-        
-    % now export segments of interest..
-%     [labels_data, segment_tags] = g_segments.read_tags(g_config.DEFAULT_TAGS_PATH);
-%     map = g_segments.match_tags(labels_data, segment_tags);    
-%     
-%     % look for trajectories of interest
-%     idx = find( arrayfun( @(t) strcmp(t.abbreviation, 'S1'), segment_tags) );
-%     if ~isempty(idx)
-%         pos = find(map(:, idx));
-% 
-%         % export them
-%         for i = 1:length(pos)
-%            g_segments.items(pos(i)).plot;
-%            set(gcf, 'Color', 'w');
-%            export_fig(fullfile(g_config.OUTPUT_DIR, ...
-%                sprintf('/segment_s%d_d%d_t%d_o%d.eps', g_segments.items(pos(i)).set, g_segments.items(pos(i)).session, ...
-%                g_segments.items(pos(i)).track, round(g_segments.items(pos(i)).offset)...
-%            )));
-%         end
-%     end 
-    
+
     close;
     
      ids = [1, 1, 103; ... 
@@ -84,10 +62,7 @@ function results_export_selected_trajectories
         clf;
         g_trajectories.items(idx).plot;
         set(gcf, 'Color', 'w');
-        %export_fig(fullfile(g_config.OUTPUT_DIR, ...
-        %   sprintf('/traj_s%d_d%d_t%d.eps', g_trajectories.items(idx).set, g_trajectories.items(idx).session, ...
-        %        g_trajectories.items(idx).track)...                
-        %   ));
+        
         export_figure(1, gcf, g_config.OUTPUT_DIR,...
                 sprintf('/traj_s%d_d%d_t%d', g_trajectories.items(idx).set, g_trajectories.items(idx).session,g_trajectories.items(idx).track));
     end
